40f4c9bb0aa54c7ab6f408abf10f57630405b2fc
Kfir Dadosh <kfir.dadosh@catonetworks.com>
2021-03-08 15:00:18 +0200

    fix message

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Server ready'))
+app.listen(3000, () => console.log('Server is ready'))
